ea3ba7024d762d03e89d5fa386e6757186d114b8
Dipanshu <dipanshuyadav9385@gmail.com>
2023-12-05 11:12:42 +0530

    Currency Converter is created

---
+++ src/customHook/useCurrencyInfo.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+const useCurrencyInfo=(currency)=>{
+    const[data,setData]=useState({})
+
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then(responce=>responce.json())
+        .then(responce=> setData(responce[currency]))
+    },[currency])
+return data;
+}
+
+export default useCurrencyInfo;
